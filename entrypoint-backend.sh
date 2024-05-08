d7c7ac060c3c63f7e62c70a387dc4df2b2d69a4b
Dorian Péron <dorianperon.i@gmail.com>
2024-05-08 19:37:11 +0200

    dev: Use the same network for all services

--- entrypoint-backend.sh
+++ entrypoint-backend.sh
@@ -6,7 +6,7 @@ export PYTHONPATH="djenius-base:devel:${PYTHONPATH}"
 
 exec python -m djenius.bin.backend \
     --logging=DEBUG \
-    --listen=0.0.0.0:8000 \
+    --listen=0.0.0.0:7001 \
     --whoosh-dir=/tmp/djraio-woosh \
     --auth=djenius_auth_dev.DevAuthProvider \
     --state-file=/tmp/djraio.pickle \
--- entrypoint-resolver.sh
+++ entrypoint-resolver.sh
@@ -6,4 +6,4 @@ export PYTHONPATH="djenius-base:devel:${PYTHONPATH}"
 
 exec python -m djenius.bin.resolver \
     --logging=DEBUG \
-    --listen=0.0.0.0:8000
+    --listen=0.0.0.0:7000
